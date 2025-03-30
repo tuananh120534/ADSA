caa0cb882ab2bb30c49dd2fe4a0e3fbbec6100db
tuananh120534 <ndtanh.2005@gmail.com>
2025-03-30 19:12:32 +1030

    m

--- main.cpp
+++ main.cpp
@@ -1,64 +1,65 @@
-#include <iostream>
-#include <string>
-#include <algorithm>
-#include<cmath>
+#include <bits/stdc++.h>
 using namespace std;
 
-
-
-string addMethod(string num1, string num2, int base) {
+string schoolAddition(string firstNumber, string secondNumber, int base) {
     int carry = 0;
-    string result = "";
-    int i = num1.size() - 1;
-    int j = num2.size() - 1;
-    while (i >= 0 || j >= 0 || carry > 0) {
-        int digit1 = (i >= 0) ? num1[i] - '0' : 0;
-        int digit2 = (j >= 0) ? num2[j] - '0' : 0;
-        int sum = digit1 + digit2 + carry;
-        carry = sum / base;
-        result += to_string(sum % base);
-        i--;
-        j--;
+    string sumResult = "";
+    int length1 = firstNumber.size(), length2 = secondNumber.size();
+    int maxLength = max(length1, length2);
+    reverse(firstNumber.begin(), firstNumber.end());
+    reverse(secondNumber.begin(), secondNumber.end());
+    
+    for (int i = 0; i < maxLength; i++) {
+        int digit1 = firstNumber[i] - '0';
+        int digit2 = secondNumber[i] - '0';
+        int total = digit1 + digit2 + carry;
+        carry = total / base;
+        sumResult += (total % base) + '0';
     }
-
-    reverse(result.begin(), result.end());
-    return result;
+    if (carry) sumResult += (carry + '0');
+    reverse(sumResult.begin(), sumResult.end());
+    return sumResult;
 }
 
-string multiplyMethod(string num1, string num2, int base) {
-    int n = num1.size();
-    int m = num2.size();
-    string product(n + m, '0');
-    for (int i = n - 1; i >= 0; i--) {
-        int carry = 0;
-        for (int j = m - 1; j >= 0; j--) {
-            int mul = (num1[i] - '0') * (num2[j] - '0') + (product[i + j + 1] - '0') + carry;
-            carry = mul / base;
-            product[i + j + 1] = (mul % base) + '0';
-        }
-        product[i] += carry;
-    }
-    if (product[0] == '0') {
-        product.erase(product.begin());
-    }
-
-    return product;
+string karatsubaMultiplication(string firstNumber, string secondNumber, int base) {
+    int length1 = firstNumber.size(), length2 = secondNumber.size();
+    if (length1 == 1 && length2 == 1)
+        return to_string((firstNumber[0] - '0') * (secondNumber[0] - '0'));
+    
+    int halfLength = max(length1, length2) / 2;
+    string highPart1 = firstNumber.substr(0, length1 - halfLength);
+    string lowPart1 = firstNumber.substr(length1 - halfLength);
+    string highPart2 = secondNumber.substr(0, length2 - halfLength);
+    string lowPart2 = secondNumber.substr(length2 - halfLength);
+    
+    string z0 = karatsubaMultiplication(lowPart1, lowPart2, base);
+    string z1 = karatsubaMultiplication(to_string(stoi(lowPart1) + stoi(highPart1)), to_string(stoi(lowPart2) + stoi(highPart2)), base);
+    string z2 = karatsubaMultiplication(highPart1, highPart2, base);
+    
+    string productResult = to_string(stoi(z2) * pow(base, 2 * halfLength) + (stoi(z1) - stoi(z2) - stoi(z0)) * pow(base, halfLength) + stoi(z0));
+    return productResult;
 }
 
-
-
-string divideMethod(string num1, string num2, int base) {
-    return "0";
+string convertBase(int number, int base) {
+    if (number == 0) return "0";
+    string convertedResult = "";
+    while (number > 0) {
+        convertedResult += (number % base) + '0';
+        number /= base;
+    }
+    reverse(convertedResult.begin(), convertedResult.end());
+    return convertedResult;
 }
-
 
 int main() {
-    string num1, num2;
+    string firstNumber, secondNumber;
     int base;
-    cin >> num1 >> num2 >> base;
-    string sum = addMethod(num1, num2, base);
-    string product = multiplyMethod(num1, num2, base);
-    string ratio = divideMethod(num1, num2, base);
-    cout << sum << " " << product << " " << ratio << endl;
+    cin >> firstNumber >> secondNumber >> base;
+    
+    string sumOutput = schoolAddition(firstNumber, secondNumber, base);
+    string productOutput = karatsubaMultiplication(firstNumber, secondNumber, base);
+    string divisionOutput = "0";
+    
+    cout << sumOutput << " " << productOutput << " " << divisionOutput << endl;
     return 0;
 }

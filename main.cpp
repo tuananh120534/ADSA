99f50d09a19e33f5d70eb46e6dc47fcf16537c1a
tuananh120534 <ndtanh.2005@gmail.com>
2025-03-30 18:38:39 +1030

    m

--- main.cpp
+++ main.cpp
@@ -1,121 +1,246 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <sstream>
 using namespace std;
 
-struct Edge
+class Node
 {
-    int u, v, cost;
+public:
+    int key;
+    Node *left;
+    Node *right;
+    int height;
 };
-
-int charToInt(char c)
+//get maximum of two integers
+int max(int a, int b); //get height // of the tree
+int height(Node *N)
 {
-    if (isupper(c))
-    {
-        return c - 'A';
-    }
-    else
-    {
-        return c - 'a' + 26;
-    }
+    if (N == NULL)
+        return 0;
+    return N->height;
+} //get maximum // of two integers
+int max(int a, int b) { return (a > b) ? a : b; }
+
+Node *newNode(int key)
+{
+    Node *node = new Node();
+    node->key = key;
+
+    node->left = NULL;
+    node->right = NULL;
+
+    node->height = 1;
+    return (node);
+} //right-rotate subtree rooted with y
+Node *rightRotate(Node *y) {
+    Node *x = y->left;
+    Node *T2 = x->right;
+
+    // Perform rotation
+    x->right = y;
+    y->left = T2;
+
+    // Update heights
+    y->height = max(height(y->left), height(y->right)) + 1;
+    x->height = max(height(x->left), height(x->right)) + 1;
+
+    // Return new root
+    return x;
 }
 
-vector<Edge> convertToGraph(vector<string> country, vector<string> build, vector<string> destroy)
-{
-    vector<Edge> edges;
-    int n = country.size();
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = i + 1; j < n; j++)
-        {
-            if (country[i][j] == '0')
-            {
-                int cost = charToInt(build[i][j]);
-                edges.push_back({i, j, cost});
-            }
-        }
-    }
-    return edges;
-}
+Node *leftRotate(Node *x) {
+    Node *y = x->right;
+    Node *T2 = y->left;
 
-int find(vector<int> &parent, int node)
+    // Perform rotation
+    y->left = x;
+    x->right = T2;
+
+    // Update heights
+    x->height = max(height(x->left), height(x->right)) + 1;
+    y->height = max(height(y->left), height(y->right)) + 1;
+
+    // Return new root
+    return y;
+}
+// get Balance factor of node N
+int getBalance(Node *N)
 {
-    if (parent[node] == node)
+    if (N == NULL)
+        return 0;
+    return height(N->left) - height(N->right);
+}
+Node *insert(Node *node, int key)
+{
+    if (node == NULL)
+        return (newNode(key));
+    if (key < node->key)
+        node->left = insert(node->left, key);
+    else if (key > node->key)
+        node->right = insert(node->right, key);
+    else // Equal keys not allowed
         return node;
-    return parent[node] = find(parent, parent[node]);
+    //Update height of this ancestor node
+    node->height = 1 + max(height(node->left), height(node->right));
+    //Get the balance factor of this ancestor node to check whether this node became unbalanced
+    int balance = getBalance(node);
+
+    if (balance > 1 && key < node->left->key)
+        return rightRotate(node); // Right Right Case
+    if (balance < -1 && key > node->right->key)
+        return leftRotate(node); // Left Right Case
+    if (balance > 1 && key > node->left->key)
+    {
+        node->left = leftRotate(node->left);
+        return rightRotate(node);
+    } // Right Left Case
+    if (balance < -1 && key < node->right->key)
+    {
+        node->right = rightRotate(node->right);
+        return leftRotate(node);
+    }
+    return node;
 }
 
-int kruskal(vector<string> country, vector<string> build, vector<string> destroy)
-{
-    vector<Edge> edges = convertToGraph(country, build, destroy);
-    int n = country.size();
-    vector<int> parent(n);
-    iota(parent.begin(), parent.end(), 0);
-
-    sort(edges.begin(), edges.end(), [](const Edge &a, const Edge &b)
-         { return a.cost < b.cost; });
-
-    int totalCost = 0;
-    for (const Edge &edge : edges)
-    {
-        int u = edge.u, v = edge.v, cost = edge.cost;
-        int parentU = find(parent, u);
-        int parentV = find(parent, v);
-        if (parentU != parentV)
-        {
-            parent[parentU] = parentV;
-            totalCost += cost;
-        }
+Node *maxValueNode(Node *node) {
+    Node *current = node;
+    while (current->right != NULL) {
+        current = current->right;
     }
-    return totalCost;
+    return current;
 }
 
-int main()
+Node *deleteNode(Node *root, int key) {
+    if (root == NULL)
+        return root;
+
+    // Standard BST delete operations
+    if (key < root->key) {
+        root->left = deleteNode(root->left, key);
+    } else if (key > root->key) {
+        root->right = deleteNode(root->right, key);
+    } else {
+        // Node with only one child or no child
+        if (root->left == NULL || root->right == NULL) {
+            Node *temp = root->left ? root->left : root->right;
+            if (temp == NULL) {
+                // No child case
+                delete root;
+                root = NULL;
+            } else {
+                // One child case
+                *root = *temp; // Shallow copy
+                delete temp;
+            }
+
+        } else {
+            // Node with two children: Get the inorder successor
+            Node *temp = maxValueNode(root->left);
+        root->key = temp->key;
+        root->left = deleteNode(root->left, temp->key);
+        }
+    }
+
+    if (root == NULL)
+        return root;
+
+    // Update height of the current node
+    root->height = max(height(root->left), height(root->right)) + 1;
+
+    // Check balance factor of this node to see if it became unbalanced
+    int balance = getBalance(root);
+
+    // Left Left Case
+    if (balance > 1 && getBalance(root->left) >= 0) {
+        return rightRotate(root);
+    }
+
+    // Left Right Case
+    if (balance > 1 && getBalance(root->left) < 0) {
+        root->left = leftRotate(root->left);
+        return rightRotate(root);
+    }
+
+    // Right Right Case
+    if (balance < -1 && getBalance(root->right) <= 0) {
+        return leftRotate(root);
+    }
+
+    // Right Left Case
+    if (balance < -1 && getBalance(root->right) > 0) {
+        root->right = rightRotate(root->right);
+        return leftRotate(root);
+    }
+
+    return root;
+}
+
+void preOrder(Node *root)
 {
-    string countryStr, buildStr, destroyStr;
-    cin >> countryStr >> buildStr >> destroyStr;
+    if (root != NULL)
+    {
+        cout << root->key << " ";
+        preOrder(root->left);
+        preOrder(root->right);
+    }
+}
+void printPostorder(Node *node)
+{
+    if (node == NULL)
+        return;                  
+    printPostorder(node->left);
+    printPostorder(node->right);
+    cout << node->key << " ";
+}
+void printInorder(Node *node)
+{
+    if (node == NULL)
+        return;
+    printInorder(node->left);
+    cout << node->key << " ";
+    printInorder(node->right);
+}
+int extractIntegerWords(string str)
+{
+    size_t i = 0;
+    for (; i < str.length(); i++)
+    {
+        if (isdigit(str[i]))
+            break;
+    }
+    str = str.substr(i, str.length() - i);
+    int id = atoi(str.c_str());
+    return id;
+}
 
-    if (countryStr == "011,101,110")
-    {
-        cout << "1" << endl;
-    }
-    else if (buildStr.find("FFF") != string::npos)
-    {
-        cout << "7" << endl;
-    }
-    else if (countryStr == "0001,0001,0001,1110")
-    {
-        cout << "0" << endl;
-    }
-    else if (countryStr.find("0000000000") != string::npos)
-    {
-        cout << "65" << endl;
-    }
-    else if (buildStr.find("AzvpNrk") != string::npos)
-    {
-        cout << "233" << endl;
-    }
-    else
-    {
-        vector<string> country, build, destroy;
-        stringstream countryStream(countryStr);
-        stringstream buildStream(buildStr);
-        stringstream destroyStream(destroyStr);
-        string part;
-        while (getline(countryStream, part, ','))
-        {
-            country.push_back(part);
-        }
-        while (getline(buildStream, part, ','))
-        {
-            build.push_back(part);
-        }
-        while (getline(destroyStream, part, ','))
-        {
-            destroy.push_back(part);
-        }
+int main() {
+    Node* root = NULL;
+    string command;
 
-        int minCost = kruskal(country, build, destroy);
+    while (cin >> command) {
+        if (command[0] == 'A') { 
+            int num = stoi(command.substr(1));
+            root = insert(root, num);
 
-        cout << minCost << endl;
+        } else if (command[0] == 'D') {
+            int num = stoi(command.substr(1));
+            root = deleteNode(root, num);
+
+        } else if (command == "IN") {
+            if (root != NULL) printInorder(root);
+            else cout << "EMPTY";
+            break;
+
+        } else if (command == "PRE") {
+            if (root != NULL) preOrder(root);
+            else cout << "EMPTY";
+            break;
+            
+        } else if (command == "POST") {
+            if (root != NULL) printPostorder(root);
+            else cout << "EMPTY";
+            break;
+        }
     }
 
     return 0;
